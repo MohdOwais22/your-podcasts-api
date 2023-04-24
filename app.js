f29b4c76c45517c94465e83d277fcb78c3920dc2
MohdOwais22 <mohdowais762@gmail.com>
2023-04-24 16:08:00 +0530

    fix/deployment/:owais

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  origin: "http://localhost:3000",
+  origin: "https://tunein-yourpodcast.onrender.com",
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -30,36 +30,6 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.get(
-//   '/auth/google',
-//   passport.authenticate('google', { scope: ['email', 'profile'] })
-// );
-
-// app.get(
-//   '/auth/google/callback',
-//   passport.authenticate('google', {
-//     successRedirect: '/protected',
-//     failureRedirect: '/auth/google/failure',
-//   })
-// );
-
-// app.get('/protected', isLoggedIn, (req, res) => {
-//   res.send(`Hello ${req.user.displayName}`);
-// });
-
-// app.get('/logout', (req, res) => {
-//   req.logout(function (err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     req.session.destroy();
-//     res.send('Goodbye!');
-//   });
-// });
-
-// app.get('/auth/google/failure', (req, res) => {
-//   res.send('Failed to authenticate..');
-// });
 
 import user from './routes/User.js';
 import podcast from './routes/podcast.js';
